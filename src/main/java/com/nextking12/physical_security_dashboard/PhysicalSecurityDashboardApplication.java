3bcc59d6e41c53da981e395b9b82805d6121978d
Edward King <king8three@gmail.com>
2026-05-22 21:24:12 -0400

    project folders

---
+++ src/main/java/com/nextking12/physical_security_dashboard/PhysicalSecurityDashboardApplication.java
@@ -0,0 +1,13 @@
+package com.nextking12.physical_security_dashboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhysicalSecurityDashboardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhysicalSecurityDashboardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nextking12/physical_security_dashboard/PhysicalSecurityDashboardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nextking12.physical_security_dashboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhysicalSecurityDashboardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
