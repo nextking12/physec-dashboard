0055a1fcb9dd09fa86fd413d1f6f88e45ce50a4e
Edward King <king8three@gmail.com>
2026-05-23 20:13:53 -0400

    added UpdateDeviceRequest, ResourceNotFoundException

---
+++ src/main/java/com/nextking12/physical_security_dashboard/exception/ResourceNotFoundException.java
@@ -0,0 +1,7 @@
+package com.nextking12.physical_security_dashboard.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
--- src/test/java/com/nextking12/physical_security_dashboard/PhysicalSecurityDashboardApplicationTests.java
+++ src/test/java/com/nextking12/physical_security_dashboard/PhysicalSecurityDashboardApplicationTests.java
@@ -2,9 +2,25 @@ package com.nextking12.physical_security_dashboard;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.DynamicPropertyRegistry;
+import org.springframework.test.context.DynamicPropertySource;
+import org.testcontainers.containers.PostgreSQLContainer;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
 
 @SpringBootTest
+@Testcontainers
 class PhysicalSecurityDashboardApplicationTests {
+
+	@Container
+	static PostgreSQLContainer<?> postgres = new PostgreSQLContainer<>("postgres:16");
+
+	@DynamicPropertySource
+	static void configurePostgres(DynamicPropertyRegistry registry) {
+		registry.add("spring.datasource.url", postgres::getJdbcUrl);
+		registry.add("spring.datasource.username", postgres::getUsername);
+		registry.add("spring.datasource.password", postgres::getPassword);
+	}
 
 	@Test
 	void contextLoads() {
