ae7264cd0d83644a65cc27690e8a6b8de260d757
Edward King <king8three@gmail.com>
2026-05-27 11:57:58 -0400

    made hibernate secure

--- src/test/java/com/nextking12/physical_security_dashboard/PhysicalSecurityDashboardApplicationTests.java
+++ src/test/java/com/nextking12/physical_security_dashboard/PhysicalSecurityDashboardApplicationTests.java
@@ -45,6 +45,7 @@ class PhysicalSecurityDashboardApplicationTests {
         registry.add("spring.datasource.url", postgres::getJdbcUrl);
         registry.add("spring.datasource.username", postgres::getUsername);
         registry.add("spring.datasource.password", postgres::getPassword);
+        registry.add("spring.jpa.hibernate.ddl-auto", () -> "create-drop");
     }
 
     @BeforeEach
